c45f47969e79d36214b4b7b8a675aba4f21ad55c
hhuangxy <hhuangxy@gmail.com>
2016-09-06 20:55:39 -0700

    Fix description offset

--- scraper.py
+++ scraper.py
@@ -10,44 +10,32 @@ def genPyldArea (area):
 
   if   area.lower() == 'burnaby':
     payload = {
-      'BCD'  : 'GV',
       'imdp' : '11',
-      'RSPP' : '5',
       'AIDL' : '248,249,250,251,253,254,255,256,257,258,259,260,261,856,857,858,262,263,264,265,266,267,268,269,270,271,272,273,274,275,276,277,278,279,280,281,282'
     }
   elif area.lower() == 'coquitlam':
     payload = {
-      'BCD'  : 'GV',
       'imdp' : '16',
-      'RSPP' : '5',
       'AIDL' : '324,325,326,327,328,329,330,331,332,333,334,335,336,337,338,339,340,341,342,343,344,428,429,430'
     }
   elif area.lower() == 'newWestminster':
     payload = {
-      'BCD'  : 'GV',
       'imdp' : '12',
-      'RSPP' : '5',
       'AIDL' : '1539,1540,283,284,285,286,287,288,289,290,291,292,293,294,295'
     }
   elif area.lower() == 'portCoquitlam':
     payload = {
-      'BCD'  : 'GV',
       'imdp' : '17',
-      'RSPP' : '5',
       'AIDL' : '1541,878,345,346,347,348,349,350,351,352,433'
     }
   elif area.lower() == 'richmond':
     payload = {
-      'BCD'  : 'GV',
       'imdp' : '13',
-      'RSPP' : '5',
       'AIDL' : '915,916,917,918,919,920,921,922,923,924,925,926,927,928,929,930,931,932,933,934,935,936,937,938,939,940,941,942,943,310'
     }
   elif area.lower() == 'vancouver':
     payload = {
-      'BCD'  : 'GV',
       'imdp' : '10',
-      'RSPP' : '5',
       'AIDL' : '233,234,236,235,237,238,239,240,241,242,243,244,245,246,247,855,432'
     }
   else:
@@ -96,7 +84,9 @@ def generatePayload (area, mnage, mxage, mnbd, mnbt, ptytid, mnprc, mxprc):
     'MNBT'  : mnbt,
     'MNPRC' : mnprc,
     'MXPRC' : mxprc,
-    'SCTP'  : 'RS'
+    'SCTP'  : 'RS',
+    'BCD'   : 'GV',
+    'RSPP'  : '5'
   }
 
   return {**payload, **genPyldArea(area), **genPyldType(ptytid)}
@@ -153,7 +143,7 @@ def stripChar (line):
   """Strip unwanted characters from line
   """
 
-  newLine =  ''.join(c for c in line if (c.isalnum() or (c in '\' /!.,:')))
+  newLine =  ''.join(c for c in line if (c.isalnum() or (c in ' \'/!:",.')))
   newLine = ' '.join(newLine.split())
   if newLine.endswith(','):
     newLine = newLine[:-1]
@@ -171,9 +161,14 @@ def parseInfo (infoDesc, listInfo):
   for d in infoDesc:
     info[d] = 'NA'
 
+  # Find starting index
+  for i, val in enumerate(listInfo):
+    if (val != '') and (not val.startswith(':')):
+      break
+
   # Fill dictionary
-  info['description'] = listInfo[4]
-  for key, val in zip(listInfo[5::2], listInfo[6::2]):
+  info['description'] = listInfo[i]
+  for key, val in zip(listInfo[(i+1)::2], listInfo[(i+2)::2]):
     for d in infoDesc:
       if d in key.lower():
         info[d] = val
@@ -269,7 +264,7 @@ def writeCsv (fName, listDict):
   return 'Ok!'
 
 
-def traversePages (area, mnage, mxage, mnbd, mnbt, ptytid, mnprc, mxprc):
+def traversePages (area, mnage, mxage, mnbd, mnbt, ptytid, mnprc, mxprc, fName):
   """Dive deep into the web
   """
 
@@ -278,6 +273,7 @@ def traversePages (area, mnage, mxage, mnbd, mnbt, ptytid, mnprc, mxprc):
 
   # Get first page
   page = getPage('http://www.realtylink.org/prop_search/Summary.cfm', payload)
+  print(page.url)
 
   # Convert to HTML
   html = etree.HTML(page.text)
@@ -301,7 +297,7 @@ def traversePages (area, mnage, mxage, mnbd, mnbt, ptytid, mnprc, mxprc):
     info[-1]['url'] = page.url
 
   # Create csv
-  writeCsv('text.csv', info)
+  writeCsv(fName, info)
 
   return 'Ok!'
 
@@ -323,9 +319,9 @@ MNBT = 0
 PTYTID = 'house'
 
 # Min/max price
-MNPRC = 200000
+MNPRC =  200000
 MXPRC = 3000000
 
 
-traversePages(AREA, MNAGE, MXAGE, MNBD, MNBT, PTYTID, MNPRC, MXPRC)
+traversePages(AREA, MNAGE, MXAGE, MNBD, MNBT, PTYTID, MNPRC, MXPRC, 'test.csv')
 
